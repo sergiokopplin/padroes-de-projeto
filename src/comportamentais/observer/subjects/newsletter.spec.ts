ed69a883c0f22a1b08450c086ba9f07dfcbf30ac
Sérgio A. Kopplin <sergiokopplin@gmail.com>
2024-11-15 20:51:34 -0300

    observer

--- src/comportamentais/observer/subjects/newsletter.spec.ts
+++ src/comportamentais/observer/subjects/newsletter.spec.ts
@@ -3,50 +3,88 @@ import { Fornecedor } from "../observers/Fornecedor";
 import { Funcionario } from "../observers/Funcionario";
 import { Newsletter } from "./Newsletter";
 import { Parceiro } from "../observers/Parceiro";
+import { EmailService } from "../services/Email";
+import { ObserverService } from "../interfaces/ObserverService";
 
 interface SutTypes {
   sut: Newsletter;
+  emailStub: ObserverService;
+  logSpy: any;
 }
 
 const makeSut = (): SutTypes => {
+  const logSpy = jest.spyOn(console, "log").mockImplementation(() => {});
+
+  const emailStub = new EmailService();
   const sut = new Newsletter();
 
   return {
     sut,
+    emailStub,
+    logSpy,
   };
 };
 
-describe("Newsletter", () => {
-  test("deve iniciar com os valores corretos", () => {
-    const { sut } = makeSut();
+describe("Newsletter Integration Test", () => {
+  test("deve funcionar corretamente em todos os casos de uso", () => {
+    const { sut, emailStub, logSpy } = makeSut();
 
-    // new Funcionario("funcionario 1", "funcionario1@email.com", sut);
-    // const funcionario2 = new Funcionario(
-    //   "funcionario 2",
-    //   "funcionario2@email.com",
-    //   sut
-    // );
+    new Funcionario({
+      nome: "funcionario 1",
+      email: "funcionario 1@email.com",
+      subject: sut,
+      emailService: emailStub,
+    });
+    const funcionario2 = new Funcionario({
+      nome: "funcionario 2",
+      email: "funcionario 2@email.com",
+      subject: sut,
+      emailService: emailStub,
+    });
 
-    // new Cliente("cliente", "cliente@email.com", sut);
-    // const parceiro = new Parceiro("parceiro", "parceiro@email.com", sut);
-    // new Fornecedor("fornecedor", "fornecedor@email.com", sut);
+    new Cliente({
+      nome: "cliente 1",
+      email: "cliente 1@email.com",
+      subject: sut,
+      emailService: emailStub,
+    });
+    const parceiro = new Parceiro({
+      nome: "parceiro 1",
+      email: "parceiro 1@email.com",
+      subject: sut,
+      emailService: emailStub,
+    });
+    new Fornecedor({
+      nome: "fornecedor 1",
+      email: "fornecedor 1@email.com",
+      subject: sut,
+      emailService: emailStub,
+    });
 
-    // sut.addMensagem("Primeira Mensagem");
+    sut.addMensagem("Primeira Mensagem");
 
-    // sut.removeObserver(funcionario2);
+    expect(logSpy).toHaveBeenCalledTimes(5);
 
-    // sut.addMensagem("Segunda Mensagem");
+    sut.removeObserver(funcionario2);
 
-    // sut.removeObserver(parceiro);
+    sut.addMensagem("Segunda Mensagem");
 
-    // sut.addMensagem("Terceira Mensagem");
+    expect(logSpy).toHaveBeenCalledTimes(9);
 
-    // sut.registerObserver(funcionario2);
+    sut.removeObserver(parceiro);
 
-    // sut.addMensagem("Quarta Mensagem");
+    sut.addMensagem("Terceira Mensagem");
 
-    // sut.notifyObserver();
+    expect(logSpy).toHaveBeenCalledTimes(12);
 
-    expect(sut.addMensagem).toBeTruthy();
+    sut.registerObserver(funcionario2);
+
+    sut.addMensagem("Quarta Mensagem");
+
+    expect(logSpy).toHaveBeenCalledTimes(16);
+
+    sut.notifyObserver();
+
+    expect(logSpy).toHaveBeenCalledTimes(20);
   });
 });
