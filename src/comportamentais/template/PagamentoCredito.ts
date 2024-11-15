3f9d3563438778b9936e2d38256639cb40424182
Sérgio A. Kopplin <sergiokopplin@gmail.com>
2024-11-15 10:20:24 -0300

    pagamento credito

--- src/comportamentais/template/PagamentoCredito.ts
+++ src/comportamentais/template/PagamentoCredito.ts
@@ -1,3 +1,38 @@
 import { Gateway } from "./Gateway";
 
-export class PagamentoCredito {}
+export class PagamentoCredito {
+  private _valor: number;
+  private gateway: Gateway;
+
+  constructor(valor: number, gateway: Gateway) {
+    this._valor = valor;
+    this.gateway = gateway;
+  }
+
+  public get valor(): number {
+    return this._valor;
+  }
+
+  public set valor(value: number) {
+    this._valor = value;
+  }
+
+  public calculaTaxa(): number {
+    return this._valor * 0.05;
+  }
+
+  public calculaDesconto(): number {
+    if (this._valor > 300) {
+      return this._valor * 0.02;
+    }
+
+    return 0;
+  }
+
+  public realizarCobranca(): boolean {
+    const valorFinal =
+      this._valor + this.calculaTaxa() - this.calculaDesconto();
+
+    return this.gateway.cobrar(valorFinal);
+  }
+}
